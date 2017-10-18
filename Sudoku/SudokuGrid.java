8cb324cb50bd7466e51c7376401a50590a68d0b2
Kathleen Near <knear@cantor.cs.nmsu.edu>
2017-10-17 19:18:59 -0600

    Added menu and formatted input

--- Sudoku/SudokuGrid.java
+++ Sudoku/SudokuGrid.java
@@ -7,8 +7,11 @@
 //		   Peterson, Jare
 
 import java.awt.*;
+import java.awt.event.KeyEvent;
+
 import javax.swing.*;
 import javax.swing.border.Border;
+import java.lang.*;
 
 final class SudokuGrid extends JPanel {
 	
@@ -16,56 +19,85 @@ final class SudokuGrid extends JPanel {
 	
 	private final JTextField[][] grid;
 	private final JPanel gridPanel;
-	private final JPanel[][] smallSquares;
+	private final JPanel buttonPanel;
+	private final JPanel[][] sectionSquares;
 	
 	SudokuGrid(int size) {
 		this.grid = new JTextField[size][size];
-		for (int y = 0; y < size; ++y) {
-			for (int x = 0; x < size; ++x) {
-				JTextField field = new JTextField();
-				grid[y][x] = field;
-			}
-		}
+		this.gridPanel = new JPanel();
+		this.buttonPanel = new JPanel();
 		
-		this.gridPanel   = new JPanel();
+		//Creates borders around each square and a thick border around each section
+		Border gridBorder = BorderFactory.createLineBorder(Color.BLACK, 1);
+		Border sectionBorder = BorderFactory.createLineBorder(Color.BLACK, 2);
 		
-		Border border = BorderFactory.createLineBorder(Color.BLACK, 1);
+		//Sets the individual box size to 60x60 pixels & the sections to sqrtxsqrt boxes
 		Dimension fieldSize = new Dimension(60, 60);
+		int sectionSize = (int) Math.sqrt(size);
 		
-		for (int y = 0; y < size; ++y) {
-			for (int x = 0; x < size; ++x) {
-				JTextField field = grid[y][x];
-				field.setBorder(border);
+		/**
+		* Loop initiates the array of text fields that makes up the grid.
+		* Each field is bordered and the text is centered.
+		**/
+		for (int row = 0; row < size; ++row) {
+			for (int col = 0; col < size; ++col) {
+				
+				JTextField field = new JTextField();
+				grid[row][col] = field;
+				
+				field.setBorder(gridBorder);
 				field.setFont(ARIAL);
+				field.setHorizontalAlignment(JTextField.CENTER);
 				field.setPreferredSize(fieldSize);
+				
+				field.addKeyListener(new java.awt.event.KeyAdapter() {
+		            public void keyPressed(java.awt.event.KeyEvent evt) {
+		            	
+		            	field.setText("");
+		            	char c = evt.getKeyChar();
+		            	
+		            	if(!Character.isDigit(c) && (c != evt.VK_BACK_SPACE))
+		            		field.setText("");
+		            }
+		            
+		        });
 			}
 		}
 		
-		int squareSize = (int) Math.sqrt(size);
-		this.gridPanel.setLayout(new GridLayout(squareSize, squareSize));
+		System.out.println("END");
 		
-		this.smallSquares = new JPanel[squareSize][squareSize];
-		Border minisquareBorder = BorderFactory.createLineBorder(Color.BLACK, 2);
+		//
+		this.sectionSquares = new JPanel[sectionSize][sectionSize];
+		this.gridPanel.setLayout(new GridLayout(sectionSize, sectionSize));
 		
-		for (int y = 0; y < squareSize; ++y) {
-			for (int x = 0; x < squareSize; ++x) {
-				JPanel p = new JPanel();
-				p.setLayout(new GridLayout(squareSize,squareSize));
-				p.setBorder(minisquareBorder);
-				smallSquares[y][x] = p;
-				gridPanel.add(p);
+		for (int row = 0; row < sectionSize; ++row) {
+			for (int col = 0; col < sectionSize; ++col) {
+				
+				JPanel panel = new JPanel();
+				panel.setLayout(new GridLayout(sectionSize, sectionSize));
+				
+				panel.setBorder(sectionBorder);
+				sectionSquares[row][col] = panel;
+				gridPanel.add(panel);
 			}
 		}
 		
 		for (int y = 0; y < size; ++y) {
 			for (int x = 0; x < size; ++x) {
-				int sqX = x / squareSize;
-				int sqY = y / squareSize;
-				smallSquares[sqY][sqX].add(grid[y][x]);
+				int sqX = x / sectionSize;
+				int sqY = y / sectionSize;
+				sectionSquares[sqY][sqX].add(grid[y][x]);
 			}
 		}
 		
 		this.setLayout(new BorderLayout());
 		this.add(gridPanel, BorderLayout.NORTH);
 	}
+	
+	/*private void limitInput(java.awt.event.KeyEvent evt, JTextField textField) {  
+		char c = evt.getKeyChar();
+		String s = Character.toString(c);
+		textField.setText("");
+	}*/
+	
 }
--- Sudoku/SudokuJFrame.java
+++ Sudoku/SudokuJFrame.java
@@ -6,7 +6,8 @@
 //		   Olivas, Tanya
 //		   Peterson, Jare
 
-import java.awt.Container;
+import java.awt.*;
+import java.awt.event.ActionEvent;
 import javax.swing.*;
 
 public class SudokuJFrame {
@@ -27,5 +28,53 @@ public class SudokuJFrame {
 		frame.setVisible(true);
 		frame.setResizable(false);			//Non-resizable
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		centerView();
+		buildMenu();
 	}
+	
+	/**
+	* Constructor
+	* @param size is the desired dimension of the puzzle
+	* Adds a Sudoku grid to the JFrame
+	**/
+	private void buildMenu() {
+        JMenuBar bar = new JMenuBar();
+        JMenu optMenu = new JMenu("Options");
+        //JMenu 
+
+        JMenuItem size4  = new JMenuItem("4x4");
+        JMenuItem grid9x9  = new JMenuItem("9x9");
+
+        optMenu.add(size4);
+        optMenu.addSeparator();
+        optMenu.add(grid9x9);
+
+        bar.add(optMenu);
+
+        size4.addActionListener((ActionEvent e) -> {
+        	//SudokuJFrame grid4x4 = new SudokuJFrame(4);
+            frame.getContentPane().removeAll();
+            frame.getContentPane().add(grid = new SudokuGrid(4));
+            frame.pack();
+            centerView();
+        });
+
+        grid9x9.addActionListener((ActionEvent e) -> {
+            frame.getContentPane().removeAll();
+            frame.getContentPane().add(grid = new SudokuGrid(9));
+            frame.pack();
+            centerView();
+        });
+        frame.setJMenuBar(bar);
+	}
+    
+
+    private void centerView() {
+        Dimension screen = Toolkit.getDefaultToolkit().getScreenSize();
+        Dimension frameSize = frame.getSize();
+
+        frame.setLocation((screen.width - frameSize.width) >> 1,
+                          (screen.height - frameSize.height) >> 1);
+    }
+
 }

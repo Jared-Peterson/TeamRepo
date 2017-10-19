6ca6f26e1004ec31c76145a6b135d50de5146674
Jared Peterson <31255970+JaredP1@users.noreply.github.com>
2017-10-18 18:32:10 -0600

    Add files via upload
    added difficulty menu, fixed sizing issue, placed initial box strings into 2d array

---
+++ SudokuGrid.java
@@ -0,0 +1,110 @@
+package blah;
+//Class Project
+//CS371 - Software Development
+//
+//Authors: McLane, Kurt
+//		   Near, Kathleen
+//		   Olivas, Tanya
+//		   Peterson, Jared
+
+import java.awt.*;
+import java.awt.event.KeyEvent;
+
+import javax.swing.*;
+import javax.swing.border.Border;
+import java.lang.*;
+
+final class SudokuGrid extends JPanel {
+	
+	private static final Font ARIAL = new Font("Arial", Font.CENTER_BASELINE, 22);
+	
+	private final JTextField[][] grid;
+	private final JPanel gridPanel;
+	private final JPanel buttonPanel;
+	private final JPanel[][] sectionSquares;
+	private final String[][] boxInput;
+	
+	SudokuGrid(int size) {
+		this.grid = new JTextField[size][size];
+		this.gridPanel = new JPanel();
+		this.buttonPanel = new JPanel();
+		this.boxInput = new String[size][size];
+		
+		//Creates borders around each square and a thick border around each section
+		Border gridBorder = BorderFactory.createLineBorder(Color.BLACK, 1);
+		Border sectionBorder = BorderFactory.createLineBorder(Color.BLACK, 2);
+		
+		//Sets the individual box size to 60x60 pixels & the sections to sqrtxsqrt boxes
+		Dimension fieldSize = new Dimension(60, 60);
+		int sectionSize = (int) Math.sqrt(size);
+		
+		/**
+		* Loop initiates the array of text fields that makes up the grid.
+		* Each field is bordered and the text is centered.
+		**/
+		for (int row = 0; row < size; ++row) {
+			for (int col = 0; col < size; ++col) {
+				
+				JTextField field = new JTextField();
+				grid[row][col] = field;
+				
+				boxInput[row][col] =  field.getText();
+				System.out.println(boxInput[row][col]);
+				
+				field.setBorder(gridBorder);
+				field.setFont(ARIAL);
+				field.setHorizontalAlignment(JTextField.CENTER);
+				field.setPreferredSize(fieldSize);
+				
+				field.addKeyListener(new java.awt.event.KeyAdapter() {
+		            public void keyPressed(java.awt.event.KeyEvent evt) {
+		            	
+		            	field.setText("");
+		            	char c = evt.getKeyChar();
+		            	
+		            	if(!Character.isDigit(c) && (c != evt.VK_BACK_SPACE))
+		            		field.setText("");
+		            }
+		            
+		        });
+			}
+		}
+		
+		System.out.println("END");
+		
+		
+		//
+		this.sectionSquares = new JPanel[sectionSize][sectionSize];
+		this.gridPanel.setLayout(new GridLayout(sectionSize, sectionSize));
+		
+		for (int row = 0; row < sectionSize; ++row) {
+			for (int col = 0; col < sectionSize; ++col) {
+				
+				JPanel panel = new JPanel();
+				panel.setLayout(new GridLayout(sectionSize, sectionSize));
+				
+				panel.setBorder(sectionBorder);
+				sectionSquares[row][col] = panel;
+				gridPanel.add(panel);
+			}
+		}
+		
+		for (int y = 0; y < size; ++y) {
+			for (int x = 0; x < size; ++x) {
+				int sqX = x / sectionSize;
+				int sqY = y / sectionSize;
+				sectionSquares[sqY][sqX].add(grid[y][x]);
+			}
+		}
+		
+		this.setLayout(new BorderLayout());
+		this.add(gridPanel, BorderLayout.NORTH);
+	}
+	
+	/*private void limitInput(java.awt.event.KeyEvent evt, JTextField textField) {  
+		char c = evt.getKeyChar();
+		String s = Character.toString(c);
+		textField.setText("");
+	}*/
+	
+}
